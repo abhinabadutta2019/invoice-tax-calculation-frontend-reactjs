81d09ee2eac3ec4c5b5513407e37316693defd48
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2024-04-20 12:28:45 +0530

    added bootstrap in navbar

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -3,22 +3,41 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <h1>
+        <h1 className="navbar-brand">
           <Link to="/">Invoicely</Link>
         </h1>
-        <ul>
-          <li>
-            <Link to="/">Create Invoice</Link>
-          </li>
-          <li>
-            <Link to="/all-invoice">Invoice List</Link>
-          </li>
-          <li>
-            <Link to="/add-tax">Tax</Link>
-          </li>
-        </ul>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Create Invoice
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/all-invoice" className="nav-link">
+                Invoice List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/add-tax" className="nav-link">
+                Tax
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
